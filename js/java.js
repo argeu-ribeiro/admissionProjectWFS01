3a3bc168eea40723003ca2b449de02e02544b799
argeu-ribeiro <ag.ribeiro07@gmail.com>
2019-08-28 10:54:01 -0300

    novas inserções

--- js/java.js
+++ js/java.js
@@ -3,8 +3,17 @@ Código JS para Formulário HTML
 Author: Argeu Ribeiro
 */
 
-
 function Submit(){
     var nome;
     nome= alert ("E-mail Cadastrado com Sucesso!");
 }
+
+function mySubmit(e) { 
+    e.preventDefault(); 
+    try {
+     someBug();
+    } catch (e) {
+     throw new Error(e.message);
+    }
+    return false;
+  }
